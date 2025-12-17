6e96bbc77da5c869010b6906876edcb2e65fd55a
bala-amypo <bala@amypo.in>
2025-12-17 04:49:47 +0000

    User commit

--- src/main/java/com/example/demo/model/AcademicEvent.java
+++ src/main/java/com/example/demo/model/AcademicEvent.java
@@ -1,84 +1,72 @@
 package com.example.academic.model;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-
-public class AcademicEvent {
+public class ClashRecord  {
     private Long id;
-    private Long branchId;
-    private String title;
-    private String eventType;
-    private LocalDate startDate;
-    private LocalDate endDate;
-    private String location;
-    private String description;
-    private LocalDateTime submittedAt;
-    public AcademicEvent(Long branchId, String title, String eventType, LocalDate startDate, LocalDate endDate,
-            String location, String description, LocalDateTime submittedAt) {
-        this.branchId = branchId;
-        this.title = title;
-        this.eventType = eventType;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.location = location;
-        this.description = description;
-        this.submittedAt = submittedAt;
+    private Long eventAId;
+    private Long eventBId;
+    private String clashType;
+    private String severity;
+    private String details;
+    private LocalDateTime detectedAt;
+    private Boolean resolved;
+    public ClashRecord(Long eventAId, Long eventBId, String clashType, String severity, String details,
+            LocalDateTime detectedAt, Boolean resolved) {
+        this.eventAId = eventAId;
+        this.eventBId = eventBId;
+        this.clashType = clashType;
+        this.severity = severity;
+        this.details = details;
+        this.detectedAt = detectedAt;
+        this.resolved = resolved;
     }
     public Long getId() {
         return id;
     }
-    public Long getBranchId() {
-        return branchId;
+    public Long getEventAId() {
+        return eventAId;
     }
-    public String getTitle() {
-        return title;
+    public Long getEventBId() {
+        return eventBId;
     }
-    public String getEventType() {
-        return eventType;
+    public String getClashType() {
+        return clashType;
     }
-    public LocalDate getStartDate() {
-        return startDate;
+    public String getSeverity() {
+        return severity;
     }
-    public LocalDate getEndDate() {
-        return endDate;
+    public String getDetails() {
+        return details;
     }
-    public String getLocation() {
-        return location;
+    public LocalDateTime getDetectedAt() {
+        return detectedAt;
     }
-    public String getDescription() {
-        return description;
-    }
-    public LocalDateTime getSubmittedAt() {
-        return submittedAt;
+    public Boolean getResolved() {
+        return resolved;
     }
     public void setId(Long id) {
         this.id = id;
     }
-    public void setBranchId(Long branchId) {
-        this.branchId = branchId;
+    public void setEventAId(Long eventAId) {
+        this.eventAId = eventAId;
     }
-    public void setTitle(String title) {
-        this.title = title;
+    public void setEventBId(Long eventBId) {
+        this.eventBId = eventBId;
     }
-    public void setEventType(String eventType) {
-        this.eventType = eventType;
+    public void setClashType(String clashType) {
+        this.clashType = clashType;
     }
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
+    public void setSeverity(String severity) {
+        this.severity = severity;
     }
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
+    public void setDetails(String details) {
+        this.details = details;
     }
-    public void setLocation(String location) {
-        this.location = location;
+    public void setDetectedAt(LocalDateTime detectedAt) {
+        this.detectedAt = detectedAt;
     }
-    public void setDescription(String description) {
-        this.description = description;
+    public void setResolved(Boolean resolved) {
+        this.resolved = resolved;
     }
-    public void setSubmittedAt(LocalDateTime submittedAt) {
-        this.submittedAt = submittedAt;
-    }
-   
-    
 }
