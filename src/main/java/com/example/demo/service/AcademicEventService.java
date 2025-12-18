1a191abaea587567f287c21301915d74c86b7f85
bala-amypo <bala@amypo.in>
2025-12-18 14:58:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/AcademicEventService.java
@@ -0,0 +1,22 @@
+package com.example.academic.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.academic.entity.AcademicEvent;
+
+@Service
+public interface AcademicEventService {
+    AcademicEvent createEvent(AcademicEvent event);
+    AcademicEvent getEventByBranch(Long branchId);
+    AcademicEvent updateEvent(Long id, AcademicEvent event);
+    AcademicEvent getEventById(Long id);
+    List<AcademicEvent>getAllEvent();
+    List<AcademicEvent> getEventsByBranch(Long branchId);
+
+
+
+
+    
+}
