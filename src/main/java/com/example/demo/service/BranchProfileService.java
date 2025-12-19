2837d91e6ec06f25f25f2436cf6621b0b791fe3b
bala-amypo <bala@amypo.in>
2025-12-19 08:55:17 +0000

    User commit

--- src/main/java/com/example/demo/service/BranchProfileService.java
+++ src/main/java/com/example/demo/service/BranchProfileService.java
@@ -1,20 +1,4 @@
-// package com.example.academic.service;
 
-// import java.util.List;
-
-// import org.springframework.stereotype.Service;
-
-// import com.example.academic.entity.BranchProfile;
-
-// @Service
-// public interface BranchProfileService {
-
-    
-//     BranchProfile createBranch(BranchProfile branch);
-//     BranchProfile updateBranchStatus(Long id,boolean active);
-//     List<BranchProfile>getAllBranches();
-//     BranchProfile getBranchById(Long id);
-//     BranchProfile findByBr
 package com.example.demo.service;
 
 import java.util.List;
