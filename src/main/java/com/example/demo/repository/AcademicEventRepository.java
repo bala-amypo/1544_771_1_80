42ead268899c7a6b32c5f5748992b966b701eca1
bala-amypo <bala@amypo.in>
2025-12-19 06:27:45 +0000

    User commit

--- src/main/java/com/example/demo/repository/AcademicEventRepository.java
+++ src/main/java/com/example/demo/repository/AcademicEventRepository.java
@@ -1,18 +1,20 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.AcademicEvent;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
+import com.example.demo.entity.AcademicEvent;
+
+
 
 @Repository
-public interface AcademicEventRepository extends JpaRepository<AcademicEvent, Long> {
+public interface AcademicEventRepository extends JpaRepository<AcademicEvent ,Long> {
 
     List<AcademicEvent> findByBranchId(Long branchId);
 
-    List<AcademicEvent> findByEventType(String eventType);
+    
+} 
+    
 
-    List<AcademicEvent> findByStartDateBetween(LocalDate start, LocalDate end);
-}
