7aacc0894797b751725fa56429266c6b7e06945f
bala-amypo <bala@amypo.in>
2025-12-21 03:16:44 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -30,7 +30,6 @@ public class UserAccount {
         this.createdAt = LocalDateTime.now();
     }
 
-    // Constructors
     public UserAccount() {}
 
     public UserAccount(String fullName, String email, String password, String role, String department) {
@@ -41,4 +40,62 @@ public class UserAccount {
         this.department = department;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
     
+}
