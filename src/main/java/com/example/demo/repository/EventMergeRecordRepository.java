bb452d80123cf38f3ae96ba70dd7243a0c4bb4dc
bala-amypo <bala@amypo.in>
2025-12-21 17:23:50 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/EventMergeRecordRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.EventMergeRecord;
+
+public interface EventMergeRecordRepository
+        extends JpaRepository<EventMergeRecord, Long> {
+
+    List<EventMergeRecord> findByMergedStartDateBetween(
+            LocalDate start, LocalDate end);
+}
