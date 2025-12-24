67c0b76a600bb6a41b114e607ebbf299c05db555
bala-amypo <bala@amypo.in>
2025-12-24 07:48:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserAccountRepository.java
+++ src/main/java/com/example/demo/repository/UserAccountRepository.java
@@ -1,10 +1,10 @@
-package com.example.demo.repository;
+// package com.example.demo.repository;
 
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.UserAccount;
+// import java.util.Optional;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import com.example.demo.entity.UserAccount;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+// public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
 
-    Optional<UserAccount> findByUsername(String username);
-}
+//     Optional<UserAccount> findByUsername(String username);
+// }
--- src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
@@ -1,19 +1,19 @@
-package com.example.demo.service;
+// package com.example.demo.service;
 
-import org.springframework.stereotype.Service;
-import com.example.demo.entity.UserAccount;
-import com.example.demo.repository.UserAccountRepository;
+// import org.springframework.stereotype.Service;
+// import com.example.demo.entity.UserAccount;
+// import com.example.demo.repository.UserAccountRepository;
 
-@Service
-public class UserAccountServiceImpl {
+// @Service
+// public class UserAccountServiceImpl {
 
-    private final UserAccountRepository repository;
+//     private final UserAccountRepository repository;
 
-    public UserAccountServiceImpl(UserAccountRepository repository) {
-        this.repository = repository;
-    }
+//     public UserAccountServiceImpl(UserAccountRepository repository) {
+//         this.repository = repository;
+//     }
 
-    public UserAccount save(UserAccount user) {
-        return repository.save(user);
-    }
-}
+//     public UserAccount save(UserAccount user) {
+//         return repository.save(user);
+//     }
+// }
