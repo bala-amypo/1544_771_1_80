7486360e7037401c1f3fbc4e243ff9dfc15ca404
bala-amypo <bala@amypo.in>
2025-12-25 13:24:28 +0000

    User commit

--- src/main/java/com/example/demo/dto/LoginRequest.java
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -1,30 +1,30 @@
-// package com.example.demo.dto;
+package com.example.demo.dto;
 
-// public class LoginRequest {
+public class LoginRequest {
 
-//     private String username;
-//     private String password;
+    private String username;
+    private String password;
 
-//     public LoginRequest() {}
+    public LoginRequest() {}
 
-//     public LoginRequest(String username, String password) {
-//         this.username = username;
-//         this.password = password;
-//     }
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
-//     public String getUsername() {
-//         return username;
-//     }
+    public String getUsername() {
+        return username;
+    }
 
-//     public void setUsername(String username) {
-//         this.username = username;
-//     }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-//     public String getPassword() {
-//         return password;
-//     }
+    public String getPassword() {
+        return password;
+    }
 
-//     public void setPassword(String password) {
-//         this.password = password;
-//     }
-// }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,30 +1,30 @@
-// package com.example.demo.dto;
+package com.example.demo.dto;
 
-// public class RegisterRequest {
+public class RegisterRequest {
 
-//     private String username;
-//     private String password;
+    private String username;
+    private String password;
 
-//     public RegisterRequest() {}
+    public RegisterRequest() {}
 
-//     public RegisterRequest(String username, String password) {
-//         this.username = username;
-//         this.password = password;
-//     }
+    public RegisterRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
-//     public String getUsername() {
-//         return username;
-//     }
+    public String getUsername() {
+        return username;
+    }
 
-//     public void setUsername(String username) {
-//         this.username = username;
-//     }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-//     public String getPassword() {
-//         return password;
-//     }
+    public String getPassword() {
+        return password;
+    }
 
-//     public void setPassword(String password) {
-//         this.password = password;
-//     }
-// }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
