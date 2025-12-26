c1e939c9e5f9f12c2d9ccbe01a8d6c5ac738967e
bala-amypo <bala@amypo.in>
2025-12-26 23:32:28 +0000

    User commit

--- src/main/java/com/example/demo/repository/ClashRecordRepository.java
+++ src/main/java/com/example/demo/repository/ClashRecordRepository.java
@@ -5,11 +5,3 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClashRecordRepository extends JpaRepository<ClashRecord, Long> {
 }
-package com.example.demo.repository;
-
-import com.example.demo.entity.HarmonizedCalendar;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HarmonizedCalendarRepository
-        extends JpaRepository<HarmonizedCalendar, Long> {
-}
--- src/main/java/com/example/demo/repository/HarmonizedCalendarRepository.java
+++ src/main/java/com/example/demo/repository/HarmonizedCalendarRepository.java
@@ -1,7 +1,8 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.ClashRecord;
+import com.example.demo.entity.HarmonizedCalendar;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClashRecordRepository extends JpaRepository<ClashRecord, Long> {
+public interface HarmonizedCalendarRepository
+        extends JpaRepository<HarmonizedCalendar, Long> {
 }
