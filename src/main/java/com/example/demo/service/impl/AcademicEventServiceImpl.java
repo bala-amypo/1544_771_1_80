875c9001aaca0df44b7acae268a67488ec44590c
bala-amypo <bala@amypo.in>
2025-12-26 16:14:29 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AcademicEventServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AcademicEventServiceImpl.java
@@ -1,18 +1,19 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.AcademicEvent;
+import com.example.demo.repository.AcademicEventRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AcademicEventServiceImpl {
 
-    public void printEventDetails(AcademicEvent event) {
+    private final AcademicEventRepository repository;
 
-        System.out.println(event.getTitle());
-        System.out.println(event.getEventType());
-        System.out.println(event.getLocation());
-        System.out.println(event.getDescription());
-        System.out.println(event.getStartDate());
-        System.out.println(event.getEndDate());
+    public AcademicEventServiceImpl(AcademicEventRepository repository) {
+        this.repository = repository;
+    }
+
+    public AcademicEvent save(AcademicEvent event) {
+        return repository.save(event);
     }
 }
