311e1c8467c6222abdebb81adf4902ab04e9cb69
bala-amypo <bala@amypo.in>
2025-12-26 17:42:20 +0000

    User commit

--- src/main/java/com/example/demo/controller/AcademicEventController.java
+++ src/main/java/com/example/demo/controller/AcademicEventController.java
@@ -1,49 +1,44 @@
-
 package com.example.demo.controller;
-
-import java.util.List;
-import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.entity.AcademicEvent;
 import com.example.demo.service.AcademicEventService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
-@CrossOrigin
 public class AcademicEventController {
 
-    private final AcademicEventService service;
+    private final AcademicEventService academicEventService;
 
-    public AcademicEventController(AcademicEventService service) {
-        this.service = service;
+    public AcademicEventController(AcademicEventService academicEventService) {
+        this.academicEventService = academicEventService;
     }
 
-    
     @PostMapping
-    public AcademicEvent createEvent(@RequestBody AcademicEvent event) {
-        return service.createEvent(event);
+    public AcademicEvent create(@RequestBody AcademicEvent event) {
+        return academicEventService.createEvent(event);
     }
 
     @PutMapping("/{id}")
-    public AcademicEvent updateEvent(@PathVariable Long id,
-    @RequestBody AcademicEvent event) {
-    return service.updateEvent(id, event);
+    public AcademicEvent update(@PathVariable Long id,
+                                @RequestBody AcademicEvent event) {
+        return academicEventService.updateEvent(id, event);
     }
 
-    
     @GetMapping("/branch/{branchId}")
-    public List<AcademicEvent> getEventsByBranch(@PathVariable Long branchId) {
-        return service.getEventsByBranch(branchId);
+    public List<AcademicEvent> getByBranch(@PathVariable Long branchId) {
+        return academicEventService.getEventsByBranch(branchId);
     }
 
     @GetMapping("/{id}")
-    public AcademicEvent getEventById(@PathVariable Long id) {
-        return service.getEventById(id);
+    public AcademicEvent getById(@PathVariable Long id) {
+        return academicEventService.getEventById(id);
     }
 
-   
     @GetMapping
-    public List<AcademicEvent> getAllEvents() {
-        return service.getAllEvents();
+    public List<AcademicEvent> getAll() {
+        return academicEventService.getAllEvents();
     }
 }
