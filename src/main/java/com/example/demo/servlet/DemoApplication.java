8f0cbb926e6bd0571181eab5d56f18b40df05600
bala-amypo <bala@amypo.in>
2025-12-26 14:40:12 +0000

    User commit

---
+++ src/main/java/com/example/demo/servlet/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication   // 🔥 Enables component scanning for com.example.demo.*
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
