a2e9ed548b563fca4dbe77ffb9f2e7a2623400a1
bala-amypo <bala@amypo.in>
2025-12-26 13:29:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/AcademicEvent.java
+++ src/main/java/com/example/demo/entity/AcademicEvent.java
@@ -15,5 +15,37 @@ public class AcademicEvent {
     private LocalDate startDate;
     private LocalDate endDate;
 
-    // getters & setters
+    /* ===== GETTERS & SETTERS (REQUIRED) ===== */
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 }
