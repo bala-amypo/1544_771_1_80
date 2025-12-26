2c59432bbde59e469d71479280dfda3de8be648b
bala-amypo <bala@amypo.in>
2025-12-26 04:09:50 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,30 +1,62 @@
-// package com.example.demo.dto;
+package com.example.demo.dto;
 
-// public class RegisterRequest {
+public class RegisterRequest {
 
-//     private String username;
-//     private String password;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    private String department;
 
-//     public RegisterRequest() {}
+    public RegisterRequest() {
+    }
 
-//     public RegisterRequest(String username, String password) {
-//         this.username = username;
-//         this.password = password;
-//     }
+    public RegisterRequest(String name, String email, String password,
+                           String role, String department) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.department = department;
+    }
 
-//     public String getUsername() {
-//         return username;
-//     }
+    public String getName() {
+        return name;
+    }
 
-//     public void setUsername(String username) {
-//         this.username = username;
-//     }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-//     public String getPassword() {
-//         return password;
-//     }
+    public String getEmail() {
+        return email;
+    }
 
-//     public void setPassword(String password) {
-//         this.password = password;
-//     }
-// }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+    
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
