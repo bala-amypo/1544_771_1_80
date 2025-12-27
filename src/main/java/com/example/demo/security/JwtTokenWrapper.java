e27b7a33d88ef16deda0b909226d1917cde647d8
bala-amypo <bala@amypo.in>
2025-12-27 09:49:01 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenWrapper.java
+++ src/main/java/com/example/demo/security/JwtTokenWrapper.java
@@ -1,32 +1,32 @@
-// package com.example.demo.security;
+package com.example.demo.security;
 
-// import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Claims;
 
-// public class JwtTokenWrapper {
+public class JwtTokenWrapper {
 
-//     private final Claims payload;
+    private final Claims payload;
 
-//     public JwtTokenWrapper(Claims payload) {
-//         this.payload = payload;
-//     }
+    public JwtTokenWrapper(Claims payload) {
+        this.payload = payload;
+    }
 
-//     // Test expects this
-//     public Claims getPayload() {
-//         return payload;
-//     }
+    // Test expects this
+    public Claims getPayload() {
+        return payload;
+    }
 
-//     // Test expects this
-//     public Claims getBody() {
-//         return payload;
-//     }
+    // Test expects this
+    public Claims getBody() {
+        return payload;
+    }
 
-//     // Test expects this
-//     public String getSubject() {
-//         return payload.getSubject();
-//     }
+    // Test expects this
+    public String getSubject() {
+        return payload.getSubject();
+    }
 
-//     // Test expects this
-//     public Object get(String key) {
-//         return payload.get(key);
-//     }
-// }
+    // Test expects this
+    public Object get(String key) {
+        return payload.get(key);
+    }
+}
