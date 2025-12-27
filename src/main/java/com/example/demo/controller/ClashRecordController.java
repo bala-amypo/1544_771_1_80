4fae98dcf615f046536b7d1b54a73530853672eb
bala-amypo <bala@amypo.in>
2025-12-27 15:00:08 +0000

    User commit

--- src/main/java/com/example/demo/controller/ClashRecordController.java
+++ src/main/java/com/example/demo/controller/ClashRecordController.java
@@ -19,7 +19,10 @@ public class ClashRecordController {
     public ClashRecordController(ClashDetectionService clashService) {
         this.clashService = clashService;
     }
-
+    @PostMapping
+    public ResponseEntity<ClashRecord> logClash(@RequestBody ClashRecord clashRecord) {
+        return ResponseEntity.ok(clashService.logClash(clashRecord));
+    }
     @GetMapping
     public ResponseEntity<List<ClashRecord>> detectClashes() {
         return ResponseEntity.ok(clashService.detectClashes());
