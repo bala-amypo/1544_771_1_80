4281ea658b8bc3bd927c419c70566116b129804d
bala-amypo <bala@amypo.in>
2025-12-27 15:36:20 +0000

    User commit

--- src/main/java/com/example/demo/entity/ClashRecord.java
+++ src/main/java/com/example/demo/entity/ClashRecord.java
@@ -51,21 +51,53 @@ public class ClashRecord {
         this.resolved = false;
     }
 
-    /* ===== GETTERS ===== */
-    public Long getId() { return id; }
-    public Long getEventAId() { return eventAId; }
-    public Long getEventBId() { return eventBId; }
-    public String getClashType() { return clashType; }
-    public String getSeverity() { return severity; }
-    public String getDetails() { return details; }
-    public LocalDateTime getDetectedAt() { return detectedAt; }
-    public boolean getResolved() { return resolved; }
-
-    /* ===== SETTERS ===== */
-    public void setEventAId(Long eventAId) { this.eventAId = eventAId; }
-    public void setEventBId(Long eventBId) { this.eventBId = eventBId; }
-    public void setClashType(String clashType) { this.clashType = clashType; }
-    public void setSeverity(String severity) { this.severity = severity; }
-    public void setDetails(String details) { this.details = details; }
-    public void setResolved(boolean resolved) { this.resolved = resolved; }
+     public Long getId() {
+        return id;
+    }
+    public Long getEventAId() {
+        return eventAId;
+    }
+    public Long getEventBId() {
+        return eventBId;
+    }
+    public String getClashType() {
+        return clashType;
+    }
+    public String getSeverity() {
+        return severity;
+    }
+    public String getDetails() {
+        return details;
+    }
+    public LocalDateTime getDetectedAt() {
+        return detectedAt;
+    }
+    public Boolean getResolved() {
+        return resolved;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setEventAId(Long eventAId) {
+        this.eventAId = eventAId;
+    }
+    public void setEventBId(Long eventBId) {
+        this.eventBId = eventBId;
+    }
+    public void setClashType(String clashType) {
+        this.clashType = clashType;
+    }
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    public void setDetectedAt(LocalDateTime detectedAt) {
+        this.detectedAt = detectedAt;
+    }
+    public void setResolved(Boolean resolved) {
+        this.resolved = resolved;
+    }
 }
+
