cc27041ec222c454fdb83b132ac8f2f409203bf2
bala-amypo <bala@amypo.in>
2025-12-28 05:26:42 +0000

    User commit

--- src/main/java/com/example/demo/repository/ClashRecordRepository.java
+++ src/main/java/com/example/demo/repository/ClashRecordRepository.java
@@ -6,11 +6,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ClashRecordRepository extends JpaRepository<ClashRecord, Long> {
-    
+      
+
+
+
+
+
     List<ClashRecord> findByEventAIdOrEventBId(Long eventAId, Long eventBId);
 
     List<ClashRecord> findByResolvedFalse();
 }
+
+
+
+
+
 // package com.example.demo.repository;
 
 // import com.example.demo.entity.ClashRecord;
